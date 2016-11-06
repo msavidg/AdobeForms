f90765532ab183a5cde5cb89c6a9265e7622fb7b
msavidg <msavidg@gmail.com>
2016-11-06 17:26:53 -0500

    Upgraded all dependencies. Working call.

---
+++ AdobeForms.Processor/XDPProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AdobeForms.Processor
+{
+    public class XDPProcessor
+    {
+        public XElement GetXDPCustomFormData(string xdpFileName)
+        {
+
+            string filePath = String.Format(@"C:\dev\FormsLibrary\EXC\Documents\{0}", xdpFileName);
+
+            XElement customFormData = new XElement("CustomFormData");
+
+            XElement xdpElement = XElement.Load(filePath);
+
+
+            return customFormData;
+        }
+    }
+}
--- AdobeForms.Web/Controllers/AdobeFormController.cs
+++ AdobeForms.Web/Controllers/AdobeFormController.cs
@@ -5,8 +5,12 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Mime;
+using System.Text;
 using System.Web;
 using System.Web.Http;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using AdobeForms.Processor;
 
 namespace AdobeForms.Web.Controllers
 {
@@ -15,17 +19,23 @@ namespace AdobeForms.Web.Controllers
 
         public HttpResponseMessage GetForm(string adobeFormName)
         {
+            StringBuilder stringBuilder = new StringBuilder();
 
-            string s = $"<div><h1>{DateTime.Now.ToString("O")}</h1></div>";
+            stringBuilder.AppendLine($"<h2>{DateTime.Now.ToString("o")}</h2>");
+
+            AdobeForms.Processor.XDPProcessor xdpProcessor = new XDPProcessor();
+
+            string s = HttpContext.Current.Server.MapPath(adobeFormName);
+
+            XElement customFormDataElement = xdpProcessor.GetXDPCustomFormData(adobeFormName);
 
             return new HttpResponseMessage()
             {
                 StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(s)
+                Content = new StringContent(stringBuilder.ToString())
                 {
                     Headers =
                     {
-                        ContentLength = s.Length,
                         ContentType =  new MediaTypeHeaderValue( MediaTypeNames.Text.Html.ToString())
                     }
                 }
