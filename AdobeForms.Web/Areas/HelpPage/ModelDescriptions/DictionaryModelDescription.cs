c9dec1e9722f6f5c86585ed57075ea990ec0f7de
msavidg <msavidg@gmail.com>
2016-11-06 08:43:36 -0500

    Basic working web api.

---
+++ AdobeForms.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AdobeForms.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AdobeForms.Web/Controllers/AdobeFormController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Web;
+using System.Web.Http;
+
+namespace AdobeForms.Web.Controllers
+{
+    public class AdobeFormController : ApiController
+    {
+
+        public HttpResponseMessage GetForm(string adobeFormName)
+        {
+
+            string s = $"<div><h1>{DateTime.Now.ToString("O")}</h1></div>";
+
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(s)
+                {
+                    Headers =
+                    {
+                        ContentLength = s.Length,
+                        ContentType =  new MediaTypeHeaderValue( MediaTypeNames.Text.Html.ToString())
+                    }
+                }
+            };
+        }
+    }
+}
